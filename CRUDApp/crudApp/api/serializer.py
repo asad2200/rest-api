ea922930450349cf5d72aaa8f189ba0e5d8cf135
asad2200 <asadjakhavala92@gmail.com>
2021-05-05 17:31:03 +0530

    validation added in CRUDApp

--- CRUDApp/crudApp/api/serializer.py
+++ CRUDApp/crudApp/api/serializer.py
@@ -2,9 +2,16 @@ from rest_framework import serializers
 from .models import Student
 
 
+# validators
+def capital(value):
+    if value[0].upper() != value[0]:
+        raise serializers.ValidationError(
+            'Name Should be start with capital letter')
+
+
 class StudentSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=100)
+    name = serializers.CharField(max_length=100, validators=[capital])
     roll_no = serializers.IntegerField()
     city = serializers.CharField(max_length=100)
 
@@ -17,3 +24,16 @@ class StudentSerializer(serializers.Serializer):
         instance.city = validated_data.get('city', instance.city)
         instance.save()
         return instance
+
+    # field level validation
+    def validate_roll_no(self, value):
+        if value >= 50:
+            raise serializers.ValidationError("Seats are full !!")
+        return value
+
+    # object level validation
+    def validate(self, data):
+        ct = data.get("city")
+        if ct.lower() != 'basu':
+            raise serializers.ValidationError('student must be belongs to basu')
+        return data
