5ffbcf9418451f72e02548b8e62fa2e5b077aad1
asad2200 <asadjakhavala92@gmail.com>
2021-05-06 16:03:48 +0530

    GenericView + mixin added

---
+++ GenericViewCrud/api/views.py
@@ -0,0 +1,36 @@
+from django.db.models import query
+from .models import Student
+from .serializer import StudentSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+
+# Create your views here.
+
+
+class LCStudentAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class RUDStudentAPI(GenericAPIView, RetrieveModelMixin, UpdateModelMixin,
+                    DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
